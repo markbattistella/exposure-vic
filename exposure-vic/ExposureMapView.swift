b317dca1396ebb5e1f44b7bed4c3a54ff8a911c4
Mark Battistella <markbattistella@users.noreply.github.com>
2021-08-17 09:40:00 +1000

    fixed coreloaction data

--- exposure-vic/ExposureMapView.swift
+++ exposure-vic/ExposureMapView.swift
@@ -8,31 +8,29 @@
 import SwiftUI
 
 struct ExposureMapView: View {
-    var body: some View {
-
-		@StateObject var locationViewModel = LocationViewModel()
+	@StateObject var locationViewModel = LocationViewModel()
+	
+	var body: some View {
 		
-		var body: some View {
-			switch locationViewModel.authorizationStatus {
-				case .notDetermined:
-					AnyView(RequestLocationView())
-						.environmentObject(locationViewModel)
-				case .restricted:
-					ErrorView(errorText: "Location use is restricted.")
-				case .denied:
-					ErrorView(errorText: "The app does not have location permissions. Please enable them in settings.")
-				case .authorizedAlways, .authorizedWhenInUse:
-					TrackingView()
-						.environmentObject(locationViewModel)
-				default:
-					Text("Unexpected status")
-			}
+		switch locationViewModel.authorizationStatus {
+			case .notDetermined:
+				AnyView(RequestLocationView())
+					.environmentObject(locationViewModel)
+			case .restricted:
+				ErrorView(errorText: "Location use is restricted.")
+			case .denied:
+				ErrorView(errorText: "The app does not have location permissions. Please enable them in settings.")
+			case .authorizedAlways, .authorizedWhenInUse:
+				TrackingView()
+					.environmentObject(locationViewModel)
+			default:
+				Text("Unexpected status")
 		}
 	}
 }
 
 struct ExposureMapView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExposureMapView()
-    }
+	static var previews: some View {
+		ExposureMapView()
+	}
 }
--- exposure-vic/MapKit/TrackingView.swift
+++ exposure-vic/MapKit/TrackingView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct TrackingView: View {
 	
